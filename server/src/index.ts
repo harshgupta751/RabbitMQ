384e056772aa485a8170fcc81405e188f307fa50
harshgupta751 <harshachieve300@gmail.com>
2026-05-17 09:21:33 +0530

    Rabbit MQ implemented

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import amqplib from 'amqplib'
+
+const queue= "Submission_QUEUE"
+
+async function sendMessage(){
+
+const connection= await amqplib.connect("amqp://localhost:5672")
+
+const channel = await connection.createChannel()
+
+await channel.assertQueue(queue,{
+    durable: true
+}
+ )
+
+for(let i=0;i<20;i++){
+  channel.sendToQueue(queue, Buffer.from(JSON.stringify({
+    submissionID: 1234
+  })), {
+  persistent: true
+})
+
+ console.log("Notification sent successfully")
+
+}
+ 
+}
+
+sendMessage()
---
+++ worker/src/index.ts
@@ -0,0 +1,31 @@
+import ampqlib, { ConsumeMessage } from 'amqplib'
+
+const queue = "Submission_QUEUE"
+
+async function Consume(){
+
+const connection = await ampqlib.connect("amqp://localhost:5672")
+
+const channel= await connection.createChannel()
+
+
+channel.prefetch(1)
+
+ channel.consume(queue, async (message)=>{
+
+    console.log("Message Received: ", message?.content)
+
+    await new Promise((resolve, reject)=> setTimeout(resolve, 3000))
+
+    channel.ack(message as ConsumeMessage)
+    console.log("Task completed")
+}
+,{
+    noAck: false
+}
+)
+
+}
+
+Consume()
+
